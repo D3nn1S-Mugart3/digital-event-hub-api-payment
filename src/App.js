6507b14fb9899ccc738c216477af516e100cc588
D3nn1S-Mugart3 <dennis.mugarte05@gmail.com>
2024-06-26 12:39:19 -0600

    first edition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+import "bootswatch/dist/lux/bootstrap.min.css";
+import "./App.css";
+
+const stripePromise = loadStripe(
+  "pk_test_51PVz1LP0Hf605oErp0Y7krZmJc8Q06rAmW2sGbGCAvzMBu4ZcxSb9qjFmb0A4yFCQMenx3BtawnDGFPajGPCrASr00fTl6fGyW"
+);
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="card card-body">
+      <img
+        src="https://tierramarketing.es/wp-content/uploads/2022/07/micro-eventos.png"
+        alt="k68 keyboard"
+        className="img-fluid"
+      />
+      <div className="form-group">
+        <CardElement className="form-control"/>
+      </div>
+      <button className="btn btn-success">buy</button>
+    </form>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Elements stripe={stripePromise}>
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-md-4 offset-md-4">
+            <CheckoutForm />
+          </div>
+        </div>
+      </div>
+    </Elements>
   );
 }
 
